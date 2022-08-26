4c9ec7c877e66b1a01c06ead1ee4401b1ac52c23
muhmand <107528622+muhmand@users.noreply.github.com>
2022-08-25 23:21:43 -0400

    Update CustomMethod1.java

--- src/javaforgit/CustomMethod1.java
+++ src/javaforgit/CustomMethod1.java
@@ -3,6 +3,10 @@ package javaforgit;
 public class CustomMethod1 {
     public static void main(String[] args) {
         // open to create a conflict
+        
+        // change on the remote level
+        
+        
 
 
 
