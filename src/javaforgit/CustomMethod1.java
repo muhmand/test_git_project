1f2b0f0f7db09f5aa16b0d1d5ba445a28b76b30c
muhmand <107528622+muhmand@users.noreply.github.com>
2022-08-25 23:23:17 -0400

    Custom Method 1 class created

--- src/javaforgit/CustomMethod1.java
+++ src/javaforgit/CustomMethod1.java
@@ -3,8 +3,14 @@ package javaforgit;
 public class CustomMethod1 {
     public static void main(String[] args) {
         // open to create a conflict
+        // just for test purpose
+        // to visualize
 
 
+
+        // commented on the local level
+
+        // 123456789
 
     }
 }
