6d9f145eb8ad734af08af029f1a40708683f95a6
muhmand <107528622+muhmand@users.noreply.github.com>
2022-08-25 22:46:11 -0400

    updated the the remote branch

--- src/javaforgit/LoopPG.java
+++ src/javaforgit/LoopPG.java
@@ -4,16 +4,11 @@ public class LoopPG {
     public static void main(String[] args) {
         int num = 5;
         for (int i = 0; i < 4; i++) {
-            
-            for(int each: num){
-                System.out.println(num);
-                
-                {
-                 // for each loop created in the dev branch on the remote level  
 
+                }
+        // for each loop created in the dev branch on the remote level
+        String str = "Hello";
 
-
-
+        // for each loop created in the dev branch on the remote level
+            }
         }
-    }
-}
